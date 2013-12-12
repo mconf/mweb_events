7fe66014749e13ce496518b12366cfcaef1767d9
Lucas Zawacki <lfzawacki@gmail.com>
2013-12-12 17:14:03 -0200

    Added rspec tests

--- lib/eventplug/engine.rb
+++ lib/eventplug/engine.rb
@@ -15,5 +15,13 @@ require 'compass-rails'
 module Eventplug
   class Engine < ::Rails::Engine
     isolate_namespace Eventplug
+
+    config.generators do |g|
+      g.test_framework :rspec, :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
+
   end
 end
--- test/unit/helpers/eventplug/events_helper_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-module Eventplug
-  class EventsHelperTest < ActionView::TestCase
-  end
-end
