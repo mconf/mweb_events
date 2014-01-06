ce552723d7f0d466052b32c9e9ca6141b71bbc57
Lucas Zawacki <lfzawacki@gmail.com>
2014-01-06 16:34:25 -0200

    Social buttons for facebook, twitter and g+. No modals yet

--- app/helpers/eventplug/events_helper.rb
+++ app/helpers/eventplug/events_helper.rb
@@ -1,4 +1,20 @@
 module Eventplug
   module EventsHelper
+
+    def googleplus_button_url
+      "https://plus.google.com/share?url=#{CGI.escape(request.original_url)}"
+    end
+
+    def twitter_button_url
+      text = I18n.t('eventplug.events.social_media.twitter_link', :url => CGI.escape(request.original_url))
+     "https://twitter.com/intent/tweet?text=#{text}"
+    end
+
+    def facebook_button_url
+      url = CGI.escape(request.original_url)
+      text = I18n.t('eventplug.events.social_media.facebook_link', :url => url)
+      "https://www.facebook.com/sharer/sharer.php?u=#{url}&t=#{text}"
+    end
+
   end
 end
