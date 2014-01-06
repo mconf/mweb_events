8fc46662a027fa42e421dfed24d376badb438d93
Lucas Zawacki <lfzawacki@gmail.com>
2014-01-06 13:07:43 -0200

    Using views flash with bootstrap

--- app/helpers/eventplug/application_helper.rb
+++ app/helpers/eventplug/application_helper.rb
@@ -6,5 +6,15 @@ module Eventplug
     def controller_name_for_view
       params[:controller].parameterize
     end
+
+    def bootstrap_class_for flash_type
+      types = {
+        :success => 'alert-success',
+        :error => 'alert-error',
+        :alert => 'alert-block',
+        :notice => 'alert-info'
+      }
+      flash_type ? types[flash_type] : flash_type.to_s
+    end
   end
 end
