82d93c50dace69a3c0d9ae17e61d3821672a259e
Lucas Zawacki <lfzawacki@gmail.com>
2014-01-28 17:36:58 -0200

    Shorter leaflet attribution

--- lib/mweb_events.rb
+++ lib/mweb_events.rb
@@ -1,7 +1,7 @@
 require "mweb_events/engine"
 
 Leaflet.tile_layer = "http://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
-Leaflet.attribution = " \u00A9 OpenStreetMap contributors <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA 2.0</a>"
+Leaflet.attribution = " \u00A9 OpenStreetMap contrib. <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA 2.0</a>"
 Leaflet.max_zoom = 15
 
 module MwebEvents
