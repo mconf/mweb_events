64bb829ac6b00a2222b6b224de8fc35d6dd43008
Lucas Zawacki <lfzawacki@gmail.com>
2015-01-26 15:59:31 -0200

    Correct events_controller update form tests to match what the view sends

--- spec/controllers/mweb_events/events_controller_spec.rb
+++ spec/controllers/mweb_events/events_controller_spec.rb
@@ -226,10 +226,10 @@ describe MwebEvents::EventsController do
       let(:attributes) { FactoryGirl.attributes_for(:event) }
       before(:each) { put :update, :id => event, :event => attributes.merge(
         {:start_on_date => attributes[:start_on].strftime('%m/%d/%Y'),
-         :start_on_time => attributes[:start_on].strftime('%H:%M'),
-         :start_on => nil,
-         :end_on => nil}
-      )}
+         :start_on_time => attributes[:start_on].strftime('%H:%M')
+        }
+        ).except(:start_on, :end_on)
+      }
 
       it "sets the correct attributes in the event"
 
